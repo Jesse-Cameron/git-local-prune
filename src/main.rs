663117a2cc2dd3b2b12449fad80601d52869f9b6
Jesse <jessec.cameron@gmail.com>
2019-02-25 20:26:01 +1100

    read .git/config file for tracking branches

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use std::process;
 use std::path::Path;
+use std::process;
+use std::fs;
+
+/**
+ * Find branches that are tracking a remote
+ */
+fn git_get_refs() {
+    let git_config = fs::read_to_string(".git/config")
+        .expect("Could not read the file");
+    println!("Git Config:\n {}", git_config);
+}
 
 /**
  * Remove the local references to the remote branches that are stale
  */
-fn git_prune() -> Result<std::process::ExitStatus, std::io::Error>{
+fn git_prune() -> Result<std::process::ExitStatus, std::io::Error> {
     process::Command::new("git")
         .args(&["remote", "prune", "origin"])
         .status()
@@ -21,4 +31,6 @@ fn main() {
         println!("Error: {}", err);
         process::exit(1);
     }
+
+    git_get_refs();
 }
