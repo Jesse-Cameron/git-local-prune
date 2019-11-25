45ed2b78405abca38e702f1468ae98b7c86bdc93
Jesse <jessec.cameron@gmail.com>
2019-11-25 21:34:04 +1100

    added Result return type to main function

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,14 @@ use std::process::Command;
 
 mod branches;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     if !Path::new(".git").exists() {
         println!("Not in a git repository, couldn't find .git directory.");
         process::exit(1);
     }
 
     // find all local branches
-    let local_branches = {
-        match branches::local::retrieve() {
-            Ok(v) => v,
-            Err(_) => panic!("Could not retrieve local branches")
-        }
-    };
+    let local_branches = branches::local::retrieve()?;
 
     // get all of the remote branches
     let remote_branches = branches::remote::retrieve();
@@ -31,30 +26,22 @@ fn main() {
     }
 
     // get the current branch and see if it up for deletion
-    match branches::local::get_current() {
-        Ok(current_branch) => {
-            if orphaned_branches.contains(&current_branch) {
-                let status = Command::new("git")
-                    .args(&["checkout", "master"])
-                    .status()
-                    .expect("failed to execute git checkout");
-                if !status.success() {
-                    process::exit(1);
-                }
-            }
-        },
-        Err(_) => {
-            println!("Error getting the current branch.");
+    let current_branch = branches::local::get_current()?;
+    if orphaned_branches.contains(&current_branch) {
+        let status = Command::new("git")
+            .args(&["checkout", "master"])
+            .status()
+            .expect("failed to execute git checkout");
+        if !status.success() {
             process::exit(1);
         }
     }
-    
+
     // delete those branches
     for branch in &orphaned_branches {
         let branch_path = branches::delete::get_path(branch);
-        match fs::remove_file(branch_path) {
-            Ok(_) => println!("Deleted Branch: {:?}", branch),
-            Err(e) => println!("Error Deleting Branch: {:?}. Error: {}", branch, e)
-        }
+        fs::remove_file(branch_path)?;
+        println!("Deleted Branch: {:?}", branch);
     }
+    Ok(())
 }
